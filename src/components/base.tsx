175c94242431535e9e3c260afd69c90e4b14f44e
Salman A <salmana.ar@gmail.com>
2023-12-25 19:38:06 +0530

    Update title

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -10,7 +10,7 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>HTMX Twitter</title>
+      <title>BETH Twitter</title>
       <script src="https://unpkg.com/htmx.org@1.9.5"></script>
       <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
       <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
@@ -23,7 +23,7 @@ export const BaseHtml = ({ children }: PropsWithChildren) => (
     </head>
     <body hx-boost="true" class="bg-dark h-screen">
       <h1 class=" bg-blue-500 p-5 text-center text-3xl font-bold text-white shadow-md">
-        HTMX Twitter
+        BETH Twitter
       </h1>
       {children}
     </body>
